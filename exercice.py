1da9c27f72b7ea113a87ab1702cd06b947779be8
krab-z <70920725+krab-z@users.noreply.github.com>
2020-09-09 10:35:01 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,7 +4,8 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        
+        ord(lettre) -= 32 
+        chr(lettre)
         resultat += lettre
     return mot
 
